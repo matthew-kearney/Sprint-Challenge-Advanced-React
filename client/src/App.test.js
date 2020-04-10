6301e25e225d1e109ddea50830d7e4d7b45a9555
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-04-10 12:59:07 -0500

    adding my code

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react';
+import PlayerCard from './components/PlayerCard';
+import DarkMode from "./components/DarkMode";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test(`render app works`, () => {
+  render(<App/>);
+});
+
+
+test(`renders Darkmode  `, () => {
+  render (< DarkMode />);
+});
+
+
+test(`renders Title  `, () => {
+  // const { getByText } = 
+  render (< App-header />);
+  // const Title = getByText(/title/i);
+  // expect(Title).toBeInTheDocument();
 });
